989fb219ae3679aa2faf8ec5e9eb4d2eccb6342c
Zarif <zarif_41@mail.ru>
2019-03-05 14:16:11 +0300

    made changes to the structure

--- js/main.js
+++ js/main.js
@@ -1,97 +1,274 @@
+function $(query) {
+  return document.querySelector(query);
+}
+// Счетчик задач
+function countTasks() {
+  const xx = $('#mainList').getElementsByTagName('li').length;
+  const dxq = $('#mainList').getElementsByClassName('checked').length;
+  const cdx = $('#ssds');
+
+  if (xx === 1) {
+    cdx.innerHTML = `${xx - dxq} item left`;
+  } else if (xx > 1 || xx < 1) {
+    cdx.innerHTML = `${xx - dxq} items left`;
+  }
+
+  return cdx;
+}
+
+// Карандаш выделения всехзадач
+function modeOnToogle() {
+  const allLi = document.getElementsByTagName('li');
+  const liChecked = document.getElementsByClassName('checked');
+
+  if (allLi.length === 0) {
+    $('#toggle-all').checked = false;
+    $('.footer').style.display = 'none';
+    $('.pen').style.display = 'none';
+    return;
+  }
+  if (allLi.length > 0) {
+    $('.pen').style.display = 'block';
+    $('.footer').style.display = 'flex';
+  }
+  if (allLi.length === liChecked.length) {
+    $('#toggle-all').checked = true;
+    countTasks();
+  } else {
+    $('#toggle-all').checked = false;
+    countTasks();
+  }
+}
+
+// Фильтры
+
+const tagLi = document.getElementsByTagName('li');
+function changeMode() {
+  const massClass = document.getElementsByClassName('control--item');
+
+  for (let i = 0; i < massClass.length; i++) {
+    if (massClass[i].classList.contains('selected')) {
+      massClass[i].classList.remove('selected');
+    }
+  }
+}
+
+// Все задачи
+function getAllTasks() {
+  for (let i = 0; i < tagLi.length; i++) {
+    tagLi[i].style.display = 'block';
+  }
+}
+
+
+// Активные задачи
+function getActiveTasks() {
+  for (let i = 0; i < tagLi.length; i++) {
+    if (tagLi[i].classList.contains('checked')) {
+      tagLi[i].style.display = 'none';
+    } else {
+      tagLi[i].style.display = 'block';
+    }
+  }
+}
+
+// Завершенные задачи
+function getCompletedTasks() {
+  for (let i = 0; i < tagLi.length; i++) {
+    if (tagLi[i].classList.contains('checked') !== true) {
+      tagLi[i].style.display = 'none';
+    } else {
+      tagLi[i].style.display = 'block';
+    }
+  }
+}
+
+// Все задачи
+$('#filter-all').addEventListener('click', function () {
+  changeMode();
+  $('#filter-all').classList.add('selected');
+  getAllTasks();
+});
+
+// Активные задачи
+$('#filter-active').addEventListener('click', function () {
+  changeMode();
+  $('#filter-active').classList.add('selected');
+  getActiveTasks();
+});
+
+// Завершенные задачи
+$('#filter-completed').addEventListener('click', function () {
+  changeMode();
+  $('#filter-completed').classList.add('selected');
+  getCompletedTasks();
+});
+
+// Показ кнопки - clearCompleted
+function clearCompleted() {
+  const listItems = document.getElementsByTagName('li');
+  let x = 0;
+
+  for (let i = 0; i < listItems.length; i++) {
+    if (listItems[i].classList.contains('checked') === true) {
+      x += 1;
+    }
+  }
+  if (x > 0) {
+    $('#clearCompleted').style.visibility = 'visible';
+  } else {
+    $('#clearCompleted').style.visibility = 'hidden';
+  }
+}
+
+// Выбор всех задач
+function toggleAll() {
+  function check() {
+    const itemToggle = document.getElementsByClassName('toggle');
+    const thatli = document.getElementsByTagName('li');
+
+    for (let i = 0; i < itemToggle.length; i++) {
+      itemToggle[i].checked = true;
+      thatli[i].classList.add('checked');
+    }
+    mass.forEach(function (item) {
+      item.check = true;
+    });
+  }
+
+  function uncheck() {
+    const itemToggle = document.getElementsByClassName('toggle');
+    const thatli = document.getElementsByTagName('li');
+
+    for (let i = 0; i < itemToggle.length; i++) {
+      itemToggle[i].checked = false;
+      thatli[i].classList.remove('checked');
+    }
+    mass.forEach(function (item) {
+      item.check = false;
+    });
+  }
+
+  // Выделяем все
+  $('#toggle-all').addEventListener('click', function () {
+    if ($('#toggle-all').checked === true) {
+      check();
+    } else {
+      uncheck();
+    }
+    if ($('#filter-active').classList.contains('selected')) {
+      getActiveTasks();
+    }
+    if ($('#filter-completed').classList.contains('selected')) {
+      getCompletedTasks();
+    }
+    countTasks();
+    clearCompleted();
+    addLocal();
+  });
+}
+
 let mass = JSON.parse(localStorage.getItem('todo')) == null ? [] : JSON.parse(localStorage.getItem('todo'));
-let generateItems = function () {
-  let element = document.getElementById('newTask');
+
+function generateItems() {
+  const element = $('#newTask');
+
   mass.forEach(function (item) {
     element.value = item.label;
+
     if ('createEvent' in document) {
-      let evt = document.createEvent('HTMLEvents');
+      const evt = document.createEvent('HTMLEvents');
       evt.initEvent('change', false, true);
       element.dispatchEvent(evt);
     } else {
       element.fireEvent('onchange');
     }
-    let lastLi = document.getElementById('mainList').lastChild;
+    const lastLi = $('#mainList').lastChild;
     lastLi.setAttribute('data-stamp', item.timeStamp);
     lastLi.dataset.stamp = item.timeStamp;
+
     if (item.check) {
       lastLi.classList.add('checked');
       lastLi.querySelector('.toggle').checked = true;
     }
   });
-  siZe();
+
+  countTasks();
   modeOnToogle();
   clearCompleted();
-};
+}
 
-let addList = function (isOriginal) {
-  if (document.getElementById('newTask').value.trim() === '') {
-    return
+// Добавление задач
+function addList(isOriginal) {
+  if ($('#newTask').value.trim() === '') {
+    return;
   }
-  let newLi = document.createElement('li');
-  let newDiv = document.createElement('div');
-  let newInp = document.createElement('input');
-  let newLab = document.createElement('label');
-  let newButt = document.createElement('button');
+  const newLi = document.createElement('li');
+  const newDiv = document.createElement('div');
+  const newInp = document.createElement('input');
+  const newLab = document.createElement('label');
+  const newButt = document.createElement('button');
   newLi.setAttribute('data-stamp', (new Date()).valueOf());
   newLi.dataset.stamp = (new Date()).valueOf();
   newButt.className = 'destroy';
   newInp.className = 'toggle';
   newInp.type = 'checkbox';
   newDiv.className = 'view';
-  newLab.innerHTML = document.getElementById('newTask').value;
-  document.getElementById('mainList').appendChild(newLi);
+  newLab.innerHTML = $('#newTask').value;
+  $('#mainList').appendChild(newLi);
   newLi.appendChild(newDiv);
   newDiv.appendChild(newInp);
   newDiv.appendChild(newLab);
   newDiv.appendChild(newButt);
 
-  const App = function () {
+  function App() {
     this.label = newLab.textContent;
     this.check = newInp.checked === true;
     this.timeStamp = newLi.dataset.stamp;
 
     function Obj(label, check, timeStamp) {
-      this.label = label,
-        this.check = check,
-        this.timeStamp = timeStamp
+      this.label = label;
+      this.check = check;
+      this.timeStamp = timeStamp;
     }
 
     const z = new Obj(this.label, this.check, this.timeStamp);
     mass.push(z);
-  };
+  }
 
   if (isOriginal) {
     App();
     addLocal();
-    siZe();
+    countTasks();
   }
 
-  // Remove Task
+  // Удалить задачу по - х
   newButt.onclick = function () {
-    let stamp = newLi.dataset.stamp;
+    const stamp = newLi.dataset.stamp;
     newLi.remove();
     mass = mass.filter(function (element) {
       return element.timeStamp != stamp;
     });
+
     addLocal();
-    siZe();
+    countTasks();
     modeOnToogle();
   };
-  if (showCompleted.classList.contains('selected')) {
-    completedCh();
+  if ($('#filter-completed').classList.contains('selected')) {
+    getCompletedTasks();
   }
 
-  // Edit label
+  // Редактирование задачи
   newLab.addEventListener('dblclick', function () {
     newLi.classList.add('editing');
-    let stamp = newLi.dataset.stamp;
-    let edit = document.createElement('input');
+    const stamp = newLi.dataset.stamp;
+    const edit = document.createElement('input');
     edit.className = 'edit';
     edit.value = newLab.textContent;
     newLi.appendChild(edit).focus();
 
-    //Добавление задачи при нажатии Enter
-    document.querySelector('.edit').addEventListener('keyup', function (event) {
+    // Добавление задачи при нажатии Enter
+    $('.edit').addEventListener('keyup', function (event) {
       if (event.keyCode === 13) {
         if (edit.value !== '') {
           newLab.innerHTML = edit.value;
@@ -102,20 +279,20 @@ let addList = function (isOriginal) {
             }
           });
           edit.remove();
-
         } else {
           newLi.remove();
           mass = mass.filter(function (element) {
             return element.timeStamp != stamp;
           });
         }
+
         addLocal();
         modeOnToogle();
       }
     });
 
-    //Добавление задачи при клике в сторону(уход фокуса с input)
-    document.querySelector('body:not(li)').addEventListener('click', function () {
+    // Добавление задачи при клике в сторону(уход фокуса с input)
+    $('body:not(li)').addEventListener('click', function () {
       if (edit.value !== '') {
         newLab.innerHTML = edit.value;
         newLi.classList.remove('editing');
@@ -131,243 +308,90 @@ let addList = function (isOriginal) {
           return element.timeStamp != stamp;
         });
       }
+
       addLocal();
       modeOnToogle();
-      document.querySelector('body:not(li)').removeEventListener('click', arguments.callee);
+      $('body:not(li)').removeEventListener('click', arguments.callee);
     });
-
   });
-// Добавление класса для отдельныхзадач
+  // Добавление класса для отдельных задач
   newInp.addEventListener('change', function () {
     if (newLi.classList.contains('checked')) {
       newLi.classList.remove('checked');
-      let stamp = newLi.dataset.stamp;
+      const stamp = newLi.dataset.stamp;
       mass.forEach(function (item) {
         if (item.timeStamp == stamp) {
           item.check = false;
         }
       });
-      if (showCompleted.classList.contains('selected')) {
-        completedCh();
+      if ($('#filter-completed').classList.contains('selected')) {
+        getCompletedTasks();
       }
     } else {
       newLi.classList.add('checked');
-      let stamp = newLi.dataset.stamp;
+      const stamp = newLi.dataset.stamp;
       mass.forEach(function (item) {
         if (item.timeStamp == stamp) {
           item.check = true;
         }
       });
-      if (showActive.classList.contains('selected')) {
-        activeCh();
+      if ($('#filter-active').classList.contains('selected')) {
+        getActiveTasks();
       }
     }
-    siZe();
+
+    countTasks();
     modeOnToogle();
     clearCompleted();
     addLocal();
   });
-};
-//карандаш выделения всехзадач
-const modeOnToogle = function () {
-  let allLi = document.getElementsByTagName('li');
-  let liChecked = document.getElementsByClassName('checked');
-  if (allLi.length === 0) {
-    document.getElementById('toggle-all').checked = false;
-    document.querySelector('.footer').style.display = 'none';
-    document.querySelector('.pen').style.display = 'none';
-    return
-  }
-  if (allLi.length > 0) {
-    document.querySelector('.pen').style.display = 'block';
-    document.querySelector('.footer').style.display = 'flex';
-  }
-  if (allLi.length === liChecked.length) {
-    document.getElementById('toggle-all').checked = true;
-    siZe();
-  } else {
-    document.getElementById('toggle-all').checked = false;
-    siZe();
-  }
-};
-modeOnToogle();
+}
 
-//Выбор всех задач
-const toggleAll = function () {
-  function check() {
-    let itemToggle = document.getElementsByClassName('toggle');
-    let thatli = document.getElementsByTagName('li');
-    for (let i = 0; i < itemToggle.length; i++) {
-      itemToggle[i].checked = true;
-      thatli[i].classList.add('checked');
-    }
-    mass.forEach(function (item) {
-      item.check = true;
-    });
-  }
+function getSome() {
+  $('#newTask').value = '';
+  countTasks();
+  modeOnToogle();
+}
 
-  function uncheck() {
-    let itemToggle = document.getElementsByClassName('toggle');
-    let thatli = document.getElementsByTagName('li');
-    for (let i = 0; i < itemToggle.length; i++) {
-      itemToggle[i].checked = false;
-      thatli[i].classList.remove('checked');
-    }
-    mass.forEach(function (item) {
-      item.check = false;
-    });
-  }
-
-  /*Выделяем все*/
-  document.getElementById('toggle-all').addEventListener('click', function () {
-    if (document.getElementById('toggle-all').checked === true) {
-      check();
-    } else {
-      uncheck();
-    }
-    if (showActive.classList.contains('selected')) {
-      activeCh();
-    }
-    if (showCompleted.classList.contains('selected')) {
-      completedCh();
-    }
-    siZe();
-    clearCompleted();
-    addLocal();
-  });
-};
-toggleAll();
-
-/*Счетчик задач*/
-let siZe = function () {
-  let xx = document.getElementById('mainList').getElementsByTagName('li').length;
-  let dxq = document.getElementById('mainList').getElementsByClassName('checked').length;
-  let cdx = document.getElementById('ssds');
-  if (xx === 1) {
-    cdx.innerHTML = xx - dxq + ' item left';
-  } else if (xx > 1 || xx < 1) {
-    cdx.innerHTML = xx - dxq + ' items left'
-  }
-  return cdx;
-};
-siZe();
-
-//Добавление задачи при нажатии Enter
-document.getElementById('newTask').addEventListener('keyup', function (event) {
-  //event.preventDefault();
+// Добавление задачи при нажатии Enter
+$('#newTask').addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
     addList();
-    document.getElementById('newTask').value = '';
-    siZe();
-    modeOnToogle();
+    getSome();
   }
 });
 
-//Добавление задачи при клике в сторону(уход фокуса с input)
-document.getElementById('newTask').addEventListener('change', function (event) {
+// Добавление задачи при клике в сторону(уход фокуса с input)
+$('#newTask').addEventListener('change', function (event) {
   addList(event.isTrusted);
-  document.getElementById('newTask').value = '';
-  siZe();
-  modeOnToogle();
+  getSome();
 });
 
-//Удаление Завершенных задач
-document.getElementById('clearCompleted').addEventListener('click', function () {
-  let compitedLi = document.getElementsByTagName('li');
+// Удаление Завершенных задач
+$('#clearCompleted').addEventListener('click', function () {
+  const compitedLi = document.getElementsByTagName('li');
+
   for (let j = 0; j < compitedLi.length; j++) {
     if (compitedLi[j].classList.contains('checked')) {
-      let stamp = compitedLi[j].dataset.stamp;
+      const stamp = compitedLi[j].dataset.stamp;
       mass = mass.filter(function (element) {
         return element.timeStamp != stamp;
       });
       addLocal();
       compitedLi[j].remove();
-      j = j - 1;
+      j -= 1;
     }
   }
+
   clearCompleted();
   modeOnToogle();
 });
 
-// Filters
-const massClass = document.getElementsByClassName('control--item');
-const tagLi = document.getElementsByTagName('li');
-
-// Show All Tasks
-const showAll = document.querySelector('#filter-all');
-showAll.addEventListener('click', function () {
-  massPer();
-  showAll.classList.add('selected');
-  allCh();
-});
-
-// Show Active
-const showActive = document.querySelector('#filter-active');
-showActive.addEventListener('click', function () {
-  massPer();
-  showActive.classList.add('selected');
-  activeCh();
-});
-
-// Show Completed
-const showCompleted = document.querySelector('#filter-completed');
-showCompleted.addEventListener('click', function () {
-  massPer();
-  showCompleted.classList.add('selected');
-  completedCh();
-});
-
-const massPer = function () {
-  for (let i = 0; i < massClass.length; i++) {
-    if (massClass[i].classList.contains('selected')) {
-      massClass[i].classList.remove('selected');
-    }
-  }
-};
-//Активные задачи
-const activeCh = () => {
-  for (let i = 0; i < tagLi.length; i++) {
-    if (tagLi[i].classList.contains('checked')) {
-      tagLi[i].style.display = 'none';
-    } else {
-      tagLi[i].style.display = 'block';
-    }
-  }
-};
-//Завершенные задачи
-const completedCh = () => {
-  for (let i = 0; i < tagLi.length; i++) {
-    if (tagLi[i].classList.contains('checked') !== true) {
-      tagLi[i].style.display = 'none';
-    } else {
-      tagLi[i].style.display = 'block';
-    }
-  }
-};
-//Все задачи
-const allCh = () => {
-  for (let i = 0; i < tagLi.length; i++) {
-    tagLi[i].style.display = 'block';
-  }
-};
-
-//Показ кнопки - clearCompleted
-const clearCompleted = function () {
-  let sas = document.getElementsByTagName('li');
-  let x = 0;
-  for (let i = 0; i < sas.length; i++) {
-    if (sas[i].classList.contains('checked') === true) {
-      x = x + 1;
-    }
-  }
-  if (x > 0) {
-    document.querySelector('#clearCompleted').style.visibility = 'visible';
-  } else {
-    document.querySelector('#clearCompleted').style.visibility = 'hidden';
-  }
-};
-
-let addLocal = function () {
+function addLocal() {
   localStorage.setItem('todo', JSON.stringify(mass));
-};
+}
+
+countTasks();
+toggleAll();
+modeOnToogle();
 generateItems();
